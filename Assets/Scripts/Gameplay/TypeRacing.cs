f4780a150808c2ac01026e13bf923103fbd85bbe
Atomic-Potato <55362397+Atomic-Potato@users.noreply.github.com>
2022-11-25 16:32:40 +0200

    Random Text Generation

--- Assets/Scripts/Gameplay/TypeRacing.cs
+++ Assets/Scripts/Gameplay/TypeRacing.cs
@@ -3,11 +3,20 @@ using TMPro;
 
 public class TypeRacing : MonoBehaviour{
 
-    
+    [SerializeField] int wordLength;
+    [SerializeField] TMP_Text longTargetText;
     [SerializeField] TMP_InputField longInputField;
 
     void Start(){
-        // MOVE THIS TO WHEN THE PLAYER JUMPS ONCE IMPLEMENTED
         longInputField.ActivateInputField();
+        longTargetText.text = GenerateWord(wordLength);
+    }
+
+    string GenerateWord(int Length){
+        string word = "";
+        for(int i=0; i < Length; i++){
+            word = word + (char)Random.Range(65, 90);
+        }
+        return word;
     }
 }
