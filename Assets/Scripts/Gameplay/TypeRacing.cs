116bb11fdb08de2684f2de239e35c16ab3300ef3
Atomic-Potato <55362397+Atomic-Potato@users.noreply.github.com>
2022-11-25 18:57:40 +0200

    Generated text matching

--- Assets/Scripts/Gameplay/TypeRacing.cs
+++ Assets/Scripts/Gameplay/TypeRacing.cs
@@ -7,9 +7,28 @@ public class TypeRacing : MonoBehaviour{
     [SerializeField] TMP_Text longTargetText;
     [SerializeField] TMP_InputField longInputField;
 
+    string prevLongText = "";
+
     void Start(){
         longInputField.ActivateInputField();
         longTargetText.text = GenerateWord(wordLength);
+    }
+
+    void Update() {
+        if(longInputField.text.Length == longTargetText.text.Length){
+            // Add to speed
+            longInputField.text = "";
+            prevLongText = "";
+            longTargetText.text = GenerateWord(wordLength);
+        }
+
+        if(prevLongText.Length != longInputField.text.Length){
+            longInputField.text = longInputField.text.ToUpper(); 
+            if(longInputField.text[longInputField.text.Length-1] != longTargetText.text[longInputField.text.Length-1])
+                longInputField.text = prevLongText; 
+            else
+                prevLongText = longInputField.text;
+        }
     }
 
     string GenerateWord(int Length){
