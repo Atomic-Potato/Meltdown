9478887fc7c1230abee535f248d0cc35f31e03e7
Atomic-Potato <55362397+Atomic-Potato@users.noreply.github.com>
2022-11-25 19:56:07 +0200

    Some sprinkle of sexiness

--- Assets/Scripts/Gameplay/TypeRacing.cs
+++ Assets/Scripts/Gameplay/TypeRacing.cs
@@ -5,18 +5,32 @@ public class TypeRacing : MonoBehaviour{
 
     [SerializeField] int wordLength;
     [SerializeField] TMP_Text longTargetText;
+    [SerializeField] TMP_Text longDisplayText;
     [SerializeField] TMP_InputField longInputField;
 
     string prevLongText = "";
 
+    void Awake() {
+    }
+
     void Start(){
-        longInputField.ActivateInputField();
         longTargetText.text = GenerateWord(wordLength);
     }
 
     void Update() {
+        //set the position of the caret at the end if it was changed        
+
+        longInputField.ActivateInputField();
+
+        if(Input.GetKeyDown(KeyCode.Backspace)){
+            longInputField.text = prevLongText;
+            // Set back the caret position to the end
+            return;
+        }
+
         if(longInputField.text.Length == longTargetText.text.Length){
             // Add to speed
+            longDisplayText.text = "";
             longInputField.text = "";
             prevLongText = "";
             longTargetText.text = GenerateWord(wordLength);
@@ -28,6 +42,10 @@ public class TypeRacing : MonoBehaviour{
                 longInputField.text = prevLongText; 
             else
                 prevLongText = longInputField.text;
+        }
+        
+        if(Input.anyKeyDown){
+            longDisplayText.text = longInputField.text;
         }
     }
 
