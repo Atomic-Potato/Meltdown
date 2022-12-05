889ca9f6ee20b4feaa110169e7c31835e10ab7bd
Atomic-Potato <55362397+Atomic-Potato@users.noreply.github.com>
2022-12-05 23:47:43 +0200

    Timer added

--- Assets/Scripts/Gameplay/GameplayLoop.cs
+++ Assets/Scripts/Gameplay/GameplayLoop.cs
@@ -8,12 +8,23 @@ public class GameplayLoop : MonoBehaviour
     [SerializeField] Transform playerTransform;
     
 
+    [HideInInspector] public float timer = 20f;
     [HideInInspector] public int currentDistance = 0;
 
     void Update(){
         currentDistance = (int)Mathf.Abs(startTransform.position.x - playerTransform.position.x);
 
-        if(currentDistance > distanceToWin)
+        if(timer > 0){
+            timer -= Time.deltaTime;
+            if(timer < 0)
+                timer = 0;
+        }
+
+        if(currentDistance > distanceToWin && timer > 0){
             Debug.Log("You Win");
+        }
+        else if(timer <= 0) {
+            Debug.Log("You Loose!");
+        }   
     }
 }
--- Assets/Scripts/Systems & World/UserInterfaceManager.cs
+++ Assets/Scripts/Systems & World/UserInterfaceManager.cs
@@ -5,6 +5,7 @@ public class UserInterfaceManager : MonoBehaviour
 {
     [SerializeField] TMP_Text distanceText;
     [SerializeField] TMP_Text speedText;
+    [SerializeField] TMP_Text timerText;
     [Space]
     [SerializeField] GameplayLoop gameplayLoop;
     [SerializeField] PlayerController playerController;
@@ -12,6 +13,6 @@ public class UserInterfaceManager : MonoBehaviour
     void Update(){
         distanceText.text = gameplayLoop.currentDistance + "m";
         speedText.text = playerController.isGrounded ? (int)PlayerController.speed + "m/s" : (int)PlayerController.velocity.magnitude + "m/s";
-         ;
+        timerText.text = ((int)gameplayLoop.timer).ToString();
     }
 }
